de0bcc87df9b5b05506e09bf5966cc39c98825af
Jünger <stefan.juenger@gesis.org>
2020-07-10 11:22:20 +0200

    new directories for slides; moved old slides; first slide drafts

---
+++ slides/xaringan_r_setup.R
@@ -0,0 +1,14 @@
+if (!require(easypackages)) {
+  install.packages("easypackages")
+}
+
+library(easypackages)
+
+easypackages::packages(
+  "knitr", "rmarkdown", "tidyverse", "readr", "readxl", "haven",# "hadley/emo",
+  prompt = FALSE
+)
+
+options(htmltools.dir.version = FALSE)
+
+opts_chunk$set(echo = TRUE, fig.align = "center")
