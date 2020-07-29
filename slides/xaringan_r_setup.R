babaf6de790559d959f9f90fc5980e8f24c6c289
Jünger <stefan.juenger@gesis.org>
2020-07-29 13:15:16 +0200

    general r setup for slides

--- slides/xaringan_r_setup.R
+++ slides/xaringan_r_setup.R
@@ -5,10 +5,11 @@ if (!require(easypackages)) {
 library(easypackages)
 
 easypackages::packages(
-  "knitr", "rmarkdown", "tidyverse", "readr", "readxl", "haven",# "hadley/emo",
+  "knitr", "rmarkdown", "tidyverse", "readr", "readxl", "haven", "hadley/emo", "xaringanExtra",
   prompt = FALSE
 )
 
 options(htmltools.dir.version = FALSE)
 
 opts_chunk$set(echo = TRUE, fig.align = "center")
+
